184726ccf9dee1292fab4a7db8975424e6d827a0
Carzuilha <26937671+Carzuilha@users.noreply.github.com>
2017-11-11 21:03:26 -0200

    Do the camera calibration (using the Zhang method).

---
+++ zhangCalibration.m
@@ -0,0 +1,40 @@
+%==========================================================================
+%                        ZHANG METHOD CALIBRATION
+%==========================================================================
+
+function [parms, error] = zhangCalibration(NUM_FEATURES, SQRE_SIZE_MM)
+
+% Initilizing values ------------------------------------------------------
+lImages = cell(NUM_FEATURES, 1);
+rImages = cell(NUM_FEATURES, 1);
+
+% Load the dataset and store it in an array -------------------------------
+for i = 1:NUM_FEATURES;
+   
+    try 
+        lImage = sprintf('calibration/lData/%d.jpg', i);
+        rImage = sprintf('calibration/rData/%d.jpg', i);
+        
+        lImages{i} = lImage;
+        rImages{i} = rImage;
+    catch
+        error = 1;
+        return;
+    end
+    
+end
+
+% Detect the checkboards and specify the world coordinates ----------------
+[imagePoints, boardSize] = detectCheckerboardPoints(lImages, rImages);
+worldPoints = generateCheckerboardPoints(boardSize, SQRE_SIZE_MM);
+
+% Calibrate the stereo camera system --------------------------------------
+parms = estimateCameraParameters(imagePoints, worldPoints);
+
+% Save parameters in a file (used in non-calibrations) --------------------
+save('parms.mat', 'parms');
+
+% Function status ---------------------------------------------------------
+error = 0;
+
+end
